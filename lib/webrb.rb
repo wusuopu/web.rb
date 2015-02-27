f9ef779ffd6a20b347c6cce04bd20cde7f60bbce
wusuopu <admin@longchangjin.cn>
2015-02-27 11:48:20 +0800

    Wrap request && response.

--- lib/webrb.rb
+++ lib/webrb.rb
@@ -11,7 +11,13 @@ module Webrb
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
-      [200, {"Content-Type" => 'text/html'}, [text]]
+
+      if controller.get_response
+        st, hd, rs = controller.get_response.to_a
+        [st, hd, [rs.body].flatten]
+      else
+        [200, {"Content-Type" => 'text/html'}, [text]]
+      end
     end
   end
 end
--- lib/webrb/controller.rb
+++ lib/webrb/controller.rb
@@ -3,6 +3,8 @@
 
 require "erubis"
 require "webrb/file_model"
+require "rack/request"
+require "rack/response"
 
 module Webrb
   class Controller
@@ -15,11 +17,33 @@ module Webrb
       filename = File.join("app", "views", controller_name, "#{view_name}.html.erb")
       template = File.read filename
       eruby = Erubis::Eruby.new template
+      instance_variables.each do |v|
+        ctx[v.to_s[1..-1]] = instance_variable_get v
+      end
       eruby.result ctx.merge(:env => @env)
     end
     def controller_name
       klass = self.class.to_s.gsub(/Controller$/, "")
       Webrb.to_underscore klass
+    end
+
+    def request
+      @request ||= Rack::Request.new(env)
+    end
+    def params
+      request.params
+    end
+
+    def response text, status=200, headers={}
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+    def get_response
+      @response
+    end
+    def render_response *args
+      response render(*args)
     end
   end
 end
